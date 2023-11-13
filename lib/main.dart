80725f3d56e8809fb542df623dcb6d9a99b98009
krasnik <development.krasnik@gmail.com>
2023-11-13 15:41:27 +0300

    Добавлена проверка приложения

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:ui';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:my_climbing_trek/bottom_navigation_page.dart';
 import 'package:my_climbing_trek/core/services/analitics/analitics_service.dart';
 import 'package:my_climbing_trek/core/services/crash_log_service/crash_log_service.dart';
@@ -21,6 +22,7 @@ import 'package:my_climbing_trek/service_locator.dart' as di;
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 
 void main() async {
   runZonedGuarded<Future<void>>(
@@ -28,6 +30,27 @@ void main() async {
       WidgetsFlutterBinding.ensureInitialized();
 
       di.configureDependencies();
+
+      await Firebase.initializeApp();
+
+      await FirebaseAppCheck.instance.activate(
+        // You can also use a `ReCaptchaEnterpriseProvider` provider instance as an
+        // argument for `webProvider`
+        webProvider: ReCaptchaV3Provider('recaptcha-v3-site-key'),
+        // Default provider for Android is the Play Integrity provider. You can use the "AndroidProvider" enum to choose
+        // your preferred provider. Choose from:
+        // 1. Debug provider
+        // 2. Safety Net provider
+        // 3. Play Integrity provider
+        androidProvider: AndroidProvider.debug,
+        // Default provider for iOS/macOS is the Device Check provider. You can use the "AppleProvider" enum to choose
+        // your preferred provider. Choose from:
+        // 1. Debug provider
+        // 2. Device Check provider
+        // 3. App Attest provider
+        // 4. App Attest provider with fallback to Device Check provider (App Attest provider is only available on iOS 14.0+, macOS 14.0+)
+        appleProvider: AppleProvider.appAttest,
+      );
 
       await di.getIt.getAsync<CrashLogService>();
 
